b07279d1a87988939c0dad481d30767ee30a3767
Elijah <elijah_patrick_panique@dlsl.edu.ph>
2025-01-23 19:01:24 +0800

    Remove github api fetching
    I'll just add it again soon if ever

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,5 @@
 import { Nav } from "@/components/home/Nav";
 import { Footer } from "@/components/home/Footer";
-import { GetData } from "@/lib/GetData";
-import { Repository } from "@/lib/types";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import {
@@ -11,19 +9,33 @@ import {
   CardContent,
   CardFooter,
 } from "@/components/ui/card";
-import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import {
+  Avatar,
+  AvatarImage,
+  AvatarFallback
+} from "@/components/ui/avatar"
 import { ArrowUpRight } from "@phosphor-icons/react/dist/ssr";
 import { Separator } from "@/components/ui/separator";
-import { SkeletonCard } from "@/components/home/Skeleton";
+
+import TheBestPictureEver from "@/components/assets/thebestpicturetoeverexist.jpg"
+
+const projects = [
+  {
+    author: "ACES",
+    title: "Minecraft",
+    description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, sapiente.",
+    iconPath: TheBestPictureEver,
+  },
+];
 
 export default async function Projects() {
-  const data = await GetData(); // Fetch data here
-
   return (
     <>
       <Nav />
       <div className="mb-20 mt-12 container lg:max-w-6xl sm:max-w-3xl max-w-md">
-        <h1 className="text-5xl sm:text-6xl font-bold mb-4 text-start">Projects</h1>
+        <h1 className="text-5xl sm:text-6xl font-bold mb-4 text-start">
+          Projects
+        </h1>
         <p className="dark:text-white/70 text-black/70 text-start">
           A collection and photo dump of projects and other activities by ACES
         </p>
@@ -31,12 +43,7 @@ export default async function Projects() {
         <div className="my-8 -space-y-3">
           <h2 className="text-3xl font-bold mb-4 text-start">Repos</h2>
           <p className="dark:text-white/70 text-black/70 text-start">
-            Repositories in the {' '}
-            {/* <Link href="https://github.com/DLSLACES" target="_blank" className="text-neutral-300 hover:underline underline-offset-2">
-              DLSLACES
-            </Link>{' '} */}
-            {' '}
-            organization
+            Projects by ACES; Games, Websites, Applications, etc.
           </p>
         </div>
         {/* Masonry layout */}
@@ -47,64 +54,47 @@ export default async function Projects() {
           }}
         >
           {/* Show skeleton loader if data is empty */}
-          {data.length === 0 ? (
-            <>
-              <SkeletonCard />
-              <SkeletonCard />
-              <SkeletonCard />
-              {/* Add more skeletons as needed */}
-            </>
-          ) : (
-            data.map((repo: Repository) => (
-              <div
-                key={repo.id}
-                className="mb-5 break-inside-avoid" // Avoid breaking cards in masonry layout
-              >
-                <Card className="group w-full">
-                  <CardHeader>
-                    <div className="flex justify-start items-center mb-4 gap-4">
-                      <Avatar>
-                        <AvatarImage src={repo.owner.avatar_url} alt="avatar" />
+          {projects.map(({ author, title, description, iconPath }) => (
+            <div
+              key={title}
+              className="mb-5 break-inside-avoid" // Avoid breaking cards in masonry layout
+            >
+              <Card className="group w-full">
+                <CardHeader>
+                  <div className="flex justify-start items-center mb-2 gap-4">
+                    <Avatar>
+                        <AvatarImage src={iconPath.src} alt="Icon" />
                         <AvatarFallback>
-                          {repo.owner.login.charAt(0)}
+                          {author.charAt(0)}
                         </AvatarFallback>
                       </Avatar>
-                      <CardTitle className="text-md font-semibold">
-                        {repo.owner.login}
-                      </CardTitle>
-                    </div>
-                  </CardHeader>
-                  <CardContent className="flex flex-col justify-start text-start space-y-4">
-                    <h3 className="font-semibold tracking-tight">{repo.name}</h3>
-                    <p className="m-0 max-w-[30ch] text-sm opacity-50">
-                      {repo.description}
-                    </p>
-                    {/* Display languages */}
-                    <div className="flex flex-wrap gap-2">
-                      {repo.languages.map((lang) => (
-                        <span
-                          key={lang.name}
-                          className="bg-gray-100 dark:bg-neutral-800/70 text-sm px-2 py-1 rounded"
-                        >
-                          {lang.name} ({lang.lines_of_code} LOC)
-                        </span>
-                      ))}
-                    </div>
-                  </CardContent>
-                  <CardFooter className="mt-4 flex justify-end">
-                    <Link
-                      href={`https://github.com/${repo.owner.login}/${repo.name}/commits`}
-                      target="_blank"
-                    >
-                      <Button className="capitalize" variant="secondary">
-                        view commits <ArrowUpRight weight="fill" />
-                      </Button>
-                    </Link>
-                  </CardFooter>
-                </Card>
-              </div>
-            ))
-          )}
+                    <CardTitle className="text-md font-semibold">
+                      {author}
+                    </CardTitle>
+                  </div>
+                </CardHeader>
+                <CardContent className="flex flex-col justify-start text-start space-y-4">
+                  <h3 className="font-semibold tracking-tight">{title}</h3>
+                  <p className="m-0 max-w-[30ch] text-sm opacity-50">
+                    {description}
+                  </p>
+                  {/* Display languages */}
+                  <div className="flex flex-wrap gap-2">
+                  </div>
+                </CardContent>
+                <CardFooter className="flex justify-end">
+                  <Link
+                    href={`https://www.youtube.com/watch?v=1KYfstXHXa0`}
+                    target="_blank"
+                  >
+                    <Button className="capitalize" variant="secondary">
+                      View Game <ArrowUpRight weight="fill" />
+                    </Button>
+                  </Link>
+                </CardFooter>
+              </Card>
+            </div>
+          ))}
         </div>
         <div className="mt-12 mb-8 -space-y-3">
           <h2 className="text-3xl font-bold mb-4 text-start">Gallery</h2>
